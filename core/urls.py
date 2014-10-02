c9ef27e1ac21fc5d7e51c58dcd42b4efb84512e3
Rahel <rhaba001@pc13-107.intern.mi.hs-rm.de>
2014-10-02 11:03:17 +0200

    templating in html-files installed

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, include, url
 from core import views
 
 urlpatterns = patterns('',
+	url(r'^impressum/$', views.impressum, name='impressum'),
+	url(r'^presse/$', views.presse, name='presse'),
+	url(r'^konzept/$', views.konzept, name='konzept'),
 	url(r'^$', views.index, name='index'),
 )
--- core/views.py
+++ core/views.py
@@ -3,6 +3,18 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
+	return redirect("index", request)
+
+def impressum(request):
+	return redirect("impressum", request)
+
+def presse(request):
+	return redirect("presse",request)
+
+def konzept(request):
+	return redirect("konzept",request)
+
+def redirect(sitename, request):
 	context = RequestContext(request)
 	context_dic = {}
-	return render_to_response('core/index.html', context_dic, context)
+	return render_to_response('core/'+sitename+'.html', context_dic, context)
