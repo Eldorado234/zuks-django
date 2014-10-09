a774cf290227ed7be622f9f20a10455cecdec155
Simon Seyer <seyer@redpeppix.de>
2014-10-09 16:04:02 +0200

    Code style

--- core/views.py
+++ core/views.py
@@ -38,7 +38,7 @@ def redirect(sitename, request):
 
 def send_contactmail(request):
 	context = RequestContext(request)
-	
+
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
 		if form.is_valid():
@@ -54,7 +54,7 @@ def send_contactmail(request):
 			print form.errors
 	else:
 		form = ContactForm()
-	
+
 	return render_to_response('core/index.html', {'form': form, 'contacts': Contact.objects.all() }, context)
 
 def subscribeToNewsletter(request):
@@ -82,7 +82,7 @@ def subscribeToNewsletter(request):
 			context_dic['error'] = e.message
 		except IntegrityError:
 			context_dic['error'] = "Für diese E-Mail-Adresse wurde bereits ein Newsletter angefordert."
-		except Exception as e:
+		except:
 			logging.exception("Newsletter subscribtion failed")
 			context_dic['error'] = "Die Anfrage konnte leider nicht bearbeitet werden. Versuche es später erneut."
 
@@ -91,7 +91,7 @@ def subscribeToNewsletter(request):
 				recp.delete()
 			except:
 				pass
-		
+
 
 	return render_to_response('core/subscribe_form.html', context_dic, context)
 
