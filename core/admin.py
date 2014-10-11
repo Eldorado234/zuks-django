0331aeb3d940a4df8b1d431af288c8478790a119
Simon Seyer <seyer@redpeppix.de>
2014-10-11 17:02:16 +0200

    Added contact mails to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import Newsletter, NewsletterRecipient
+from core.models import Newsletter, NewsletterRecipient, ContactMail
 
 admin.site.register(Newsletter)
 admin.site.register(NewsletterRecipient)
+admin.site.register(ContactMail)
