57495448800da6954f0e71431dd55aa6799e3eba
Simon Seyer <seyer@redpeppix.de>
2014-10-11 15:28:23 +0200

    Removed javascript logic for contact form
    It is now handled on the server side

--- static/js/custom.js
+++ static/js/custom.js
@@ -8,7 +8,7 @@ $(function () {
         if(!$(this).parent('li').hasClass('active')) {
             $(this).parent('li').addClass('active');
         }
-        
+
         $(this).parent('li').siblings('.active').removeClass('active');
     });
 
@@ -26,37 +26,8 @@ $(function () {
 
     $('#jump2top').smoothScroll();
 
-    $('#button-send').submit(function() {
-        return false;
-    });
-
-    //setup email here
-    $('#button-send').click(function(event) {
-        $('#button-send').html('Sending E-Mail...');
-        event.preventDefault();
-
-        $.ajax({
-            type: 'POST',
-            url: 'send_form_email.php',
-            data: $('#contactform').serialize(),
-            contentType: 'application/x-www-form-urlencoded',
-            success: function (html) {
-                $('#contactform .success').hide();
-                $('#contactform .error').hide();
-                if (html.success == '1') {
-                    $('#contactform #button-send').html('Nachricht senden');
-                    $('#contactform .success').show();
-                }
-                else {
-                    $('#contactform #button-send').html('Nachricht senden');
-                    $('#contactform .error').show();
-                }
-            },
-            error: function () {
-                $('#contactform #button-send').html('Nachricht senden');
-                $('#contactform .error').show();
-            }
-        });
+    $('#contactform').ajaxForm(function(returnVal) {
+        $('#contactform').html($(returnVal).html());
     });
 
     $('#newsletterform').ajaxForm(function(returnVal) {
