749c232927c4ee928e92f7ad994e5c933940f5a5
Simon Seyer <seyer@redpeppix.de>
2014-10-11 21:49:25 +0200

    Improved admin rendering of some models

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from core.models import Newsletter, NewsletterRecipient, ContactMail
 
-admin.site.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+  list_display = ('subject', 'send_date')
+
+class ContactMailAdmin(admin.ModelAdmin):
+	list_display = ('name', 'sendersubject', 'sender', 'contact_date')
+
+admin.site.register(Newsletter, NewsletterAdmin)
 admin.site.register(NewsletterRecipient)
-admin.site.register(ContactMail)
+admin.site.register(ContactMail, ContactMailAdmin)
